fc169553512b31843558b225d4fb13a900e2bd55
Nicholas Vogler <Nicholas Vogler>
2017-03-12 20:53:46 -0400

    Updated file versions, added yml

--- rpn.py
+++ rpn.py
@@ -1,19 +1,28 @@
 #!/usr/bin/env python3
-
+import operator
+OPERATORS = {
+	'+': operator.add,
+	'-': operator.sub,
+	'*': operator.mul,
+	'/': operator.truediv,
+}
 def calculate(arg):
 	stack = list()
-	
-	for val in arg:
-		if val == '-':
-			return (stack.pop() - stack.pop()) * (-1)
-		elif val == '+':
-			return (stack.pop() + stack.pop())
-		else:
-			stack.append(arg)
-	
+	for operand in arg.split():
+		try:
+			operand = float(operand)
+			stack.append(operand)
+		except:
+			arg2 = stack.pop()
+			arg1 = stack.pop()
+			operator_fn = OPERATORS[operand]
+			result = operator_fn(arg1, arg2)
+			
+			stack.append(result)
+	return stack.pop()
 def main():
 	while True:
-		calculate(input("rpn calc> "))
-		
-if __name__ == '__main__': # Note: that's "underscore underscore n a m e ..."
+		result = calculate(input('rpn calc> '))
+		print("Result:", result)
+if __name__ == '__main__':
 	main()
